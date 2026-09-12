d4fb5a266002314f8e1ecd790b31816e568d48c5
Rakshak <rakshakrnaik29@gmail.com>
2026-09-12 11:02:49 +0530

    Revert: Restore original projects page - back to original state

--- src/app/(sub pages)/projects/page.js
+++ src/app/(sub pages)/projects/page.js
@@ -2,6 +2,13 @@ import Image from "next/image";
 import bg from "../../../../public/background/unsplash_earth.jpg";
 import ProjectList from "@/components/projects";
 import { projectsData } from "../../data";
+import RenderModel from "@/components/RenderModel";
+//import EarthCanvas from "@/components/models/EarthCanvas";
+import dynamic from "next/dynamic";
+
+// const EarthCanvas = dynamic(() => import("@/components/models/Earth"), {
+//   ssr: false,
+// });
 
 export const metadata = {
   title: "Projects",
@@ -19,6 +26,12 @@ export default function Home() {
       />
 
       <ProjectList projects={projectsData} />
+
+      <div className="flex items-center justify-center fixed  top-16  lg:top-20 -translate-x-1/2 lg:translate-x-0 -z-10 left-1/2 lg:-left-24 h-screen">
+        <RenderModel>
+          
+        </RenderModel>
+      </div>
     </>
   );
 }
